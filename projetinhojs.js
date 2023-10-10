068bdc7fe55babf38e1888f2c8a149d38015db4f
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2023-10-09 21:01:24 -0300

    Add files via upload

---
+++ projetinhojs.js
@@ -0,0 +1,29 @@
+var name;
+var age;
+var email;
+var situation;
+
+function initialize(){
+    name = document.getElementById("name").value;
+    age = document.getElementById("age").value;
+    email = document.getElementById("email").value; 
+}
+
+function show(){
+    document.getElementById("tableName").innerHTML = name;
+    document.getElementById("tableAge").innerHTML = age;
+    document.getElementById("tableEmail").innerHTML = email;
+    situation = document.getElementById("tableSituation");
+
+    if(age >= 25){
+        document.getElementById("tableSituation").innerHTML = "Aprovado";
+        situation.style.backgroundColor = "green";
+    } else {
+        document.getElementById("tableSituation").innerHTML = "Reprovado";
+        situation.style.backgroundColor = "red";
+    }
+}
+
+
+
+
