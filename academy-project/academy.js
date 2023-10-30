5a603b12449a53e8dabee1ba971ea7d71e4d06b5
Marcos Augusto <marcosaugusto7533@gmail.com>
2023-10-30 01:12:04 -0300

    Update academy.js

--- academy-project/academy.js
+++ academy-project/academy.js
@@ -1,37 +1,17 @@
-var stdName = [], stdAge = [], stdWeight = [], stdHeight = [], stdDAte = [], stdZP = [], stdNumber = [], stdComplement = [], stdTelephone = [], stdEmail = [];
-/*var register = {
-
-    stdNames = "Mar
-    stdAges: [stdAge],
-    stdWeights: [stdWeight],
-    stdHeight: [stdHeight],
-    stdDAtes: [stdDAte],
-    stdZPs: [stdZP],
-    stdNumbers: [stdNumber],
-    stdComplements: [stdComplement],
-    stdTelephones: [stdTelephone],
-    stdEmails: [stdEmail]
-};*/
+var stdName, stdAge, stdWeight, stdHeight, stdDAte, stdZP, stdNumber, stdComplement, stdTelephone, stdEmail;
 
 function stdRegister(){
     
-    stdName.push(document.getElementById("std-name").value);
-    /*stdAge = document.getElementById("std-age").push(value);
-    stdWeight = document.getElementById("std-weight").push(value);
-    stdHeight = document.getElementById("std-height").push(value);
-    stdDAte = document.getElementById("std-date").push(value);
-    stdZP = document.getElementById("std-zp").push(value);
-    stdNumber = document.getElementById("std-number").push(value);
-    stdComplement = document.getElementById("std-complement").push(value);
-    stdTelephone = document.getElementById("std-telephone").push(value);
-    stdEmail = document.getElementById("std-email").push(value);*/
-    if(stdName != ""){
-        const teste = document.createElement("td");
-        document.getElementById("teste1").appendChild(teste);
-
-        document.getElementById("std-bd-name").innerHTML = stdName;
-  
-    }
+    stdName = document.getElementById("std-name").value;
+    stdAge = document.getElementById("std-age").value;
+    stdWeight = document.getElementById("std-weight").value;
+    stdHeight = document.getElementById("std-height").value;
+    stdDAte = document.getElementById("std-date").value;
+    stdZP = document.getElementById("std-zp").value;
+    stdNumber = document.getElementById("std-number").value;
+    stdComplement = document.getElementById("std-complement").value;
+    stdTelephone = document.getElementById("std-telephone").value;
+    stdEmail = document.getElementById("std-email").value;
 }
 
 function consultStudents(){
@@ -41,7 +21,8 @@ function consultStudents(){
     std_form.style.display = "none";
     std_bd.style.visibility = "visible";
 
-    /*document.getElementById("std-bd-age").innerHTML = stdAge;
+    document.getElementById("std-bd-name").innerHTML = stdName;
+    document.getElementById("std-bd-age").innerHTML = stdAge;
     document.getElementById("std-bd-weight").innerHTML = stdWeight;
     document.getElementById("std-bd-height").innerHTML = stdHeight;
     document.getElementById("std-bd-date").innerHTML = stdDAte;
@@ -49,5 +30,5 @@ function consultStudents(){
     document.getElementById("std-bd-number").innerHTML = stdNumber;
     document.getElementById("std-bd-complement").innerHTML = stdComplement;
     document.getElementById("std-bd-telephone").innerHTML = stdTelephone;
-    document.getElementById("std-bd-email").innerHTML = stdEmail;*/
+    document.getElementById("std-bd-email").innerHTML = stdEmail;
 } 
