aeed355fd99e5a0a6c6c0d4ed4ca99ebccbf9c84
Marcos Augusto <88205157+MarcosStark@users.noreply.github.com>
2023-11-16 23:53:31 -0300

    Create delete_training.php

---
+++ academy-project/delete_training.php
@@ -0,0 +1,22 @@
+<?php
+
+	require_once('db_connection.php');
+
+	$id = $_GET['id'];
+
+	$objDb = new db();
+	$link = $objDb->conecta_mysql();
+
+	$sql = "DELETE FROM training WHERE id=$id";  
+	mysqli_query($link, $sql);
+
+	//executar a query
+	/*if(mysqli_query($link, $sql)){
+		echo 'Usuário registrado com sucesso!';
+	} else {
+		echo 'Erro ao registrar o usuário!';
+	}*/
+	header("Location: consult_training.php");
+
+
+?>	
